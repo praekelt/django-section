384d5bd969fd5d30b065ab6e42448f7badfe1e9e
shaunsephton <shaunsephton@gmail.com>
2010-06-15 09:13:03 +0200

    polish

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-section',
-    version='dev',
+    version='0.0.1',
     description='Django app for determining site section by request.',
-    author='Praekelt Consulting',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
     author_email='dev@praekelt.com',
+    license='BSD',
     url='http://github.com/praekelt/django-section',
     packages = find_packages(),
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
