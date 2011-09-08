aa06cb9e37bbc2ac7f8db2103320271b08a89bc5
Shaun Sephton <shaunsephton@gmail.com>
2011-09-08 11:07:52 +0200

    use latest setuptest

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from setuptools import setup, find_packages
-from setuptools.command.test import test
-
-def run_tests(self):
-    from setuptest.runtests import runtests
-    return runtests(self)
-test.run_tests = run_tests
 
 setup(
     name='django-section',
@@ -17,12 +11,12 @@ setup(
     url='http://github.com/praekelt/django-section',
     packages=find_packages(),
     include_package_data=True,
-    test_suite='section.tests',
+    test_suite='setuptest.SetupTestSuite',
     install_requires=[
         'django-snippetscream',
     ],
     tests_require=[
-        'django-setuptest',
+        'django-setuptest>=0.0.6',
     ],
     classifiers=[
         "Programming Language :: Python",
